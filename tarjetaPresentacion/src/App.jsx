b955e9abf4ae1337e21d57b76a4e739c9f08d5ee
102guss <brown_321@live.com>
2025-05-12 02:08:46 -0600

    Implementación de código sugerido.

--- tarjetaPresentacion/src/App.jsx
+++ tarjetaPresentacion/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Tarjeta from './Tarjeta'
 
 function App () {
   return (
-    <>
-
-    </>
+    <div>
+      <h1>Tarjeta de Presentación</h1>
+      {/* Renderizamos el componente Tarjeta */}
+      <Tarjeta />
+    </div>
   )
 }
 
